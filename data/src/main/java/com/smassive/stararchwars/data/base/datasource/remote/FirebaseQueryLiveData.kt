a6b4d17918761d5e46558407f95edaa81b1c489c
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2018-01-11 19:44:19 +0100

    Create `FirebaseQueryLiveData` to retrieve data from `Firebase` along with `LiveData`

---
+++ data/src/main/java/com/smassive/stararchwars/data/base/datasource/remote/FirebaseQueryLiveData.kt
@@ -0,0 +1,38 @@
+package com.smassive.stararchwars.data.base.datasource.remote
+
+import android.arch.lifecycle.LiveData
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.Query
+import com.google.firebase.database.ValueEventListener
+
+class FirebaseQueryLiveData(val query: Query) : LiveData<DataSnapshot>() {
+
+  companion object {
+    private const val LOG_TAG = "FirebaseQueryLiveData"
+  }
+
+  private val listener = DataValueEventListener()
+
+  override fun onActive() {
+    Log.d(LOG_TAG, "onActive")
+    query.addValueEventListener(listener)
+  }
+
+  override fun onInactive() {
+    Log.d(LOG_TAG, "onInactive")
+    query.removeEventListener(listener)
+  }
+
+  private inner class DataValueEventListener : ValueEventListener {
+    override fun onDataChange(dataSnapshot: DataSnapshot) {
+      value = dataSnapshot
+    }
+
+    override fun onCancelled(databaseError: DatabaseError) {
+      Log.e(LOG_TAG, "Can't listen to query " + query, databaseError.toException())
+    }
+  }
+
+}
