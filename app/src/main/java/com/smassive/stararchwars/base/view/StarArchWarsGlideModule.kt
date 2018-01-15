41fbb647e6db7bb5b125e7cbe3b03d6e3eba8be1
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2018-01-15 19:09:58 +0100

    Create our own glide module to generate GlideApp

---
+++ app/src/main/java/com/smassive/stararchwars/base/view/StarArchWarsGlideModule.kt
@@ -0,0 +1,7 @@
+package com.smassive.stararchwars.base.view
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class StarArchWarsGlideModule : AppGlideModule()
