96b9ee28c57538b33f7acbb1c2c8789fe80a5598
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2018-01-16 18:27:33 +0100

    Add a simple loading view

--- app/src/main/java/com/smassive/stararchwars/films/view/FilmsActivity.kt
+++ app/src/main/java/com/smassive/stararchwars/films/view/FilmsActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.smassive.stararchwars.R
 import com.smassive.stararchwars.data.base.extensions.observeNonNull
 import com.smassive.stararchwars.data.films.datasource.FilmsFirebaseSource
@@ -11,6 +12,7 @@ import com.smassive.stararchwars.data.films.repository.FilmsRepository
 import com.smassive.stararchwars.films.viewmodel.FilmsViewModel
 import kotlinx.android.synthetic.main.activity_films.toolbar
 import kotlinx.android.synthetic.main.content_films.filmsList
+import kotlinx.android.synthetic.main.content_films.filmsLoading
 
 class FilmsActivity : AppCompatActivity() {
 
@@ -22,14 +24,35 @@ class FilmsActivity : AppCompatActivity() {
     setContentView(R.layout.activity_films)
     setSupportActionBar(toolbar)
 
+    showLoading()
+    configList()
+    configViewModel()
+  }
+
+  private fun showLoading() {
+    filmsList.visibility = View.GONE
+    filmsLoading.visibility = View.VISIBLE
+  }
+
+  private fun hideLoading() {
+    filmsList.visibility = View.VISIBLE
+    filmsLoading.visibility = View.GONE
+  }
+
+  private fun configList() {
     filmsList.apply {
       layoutManager = LinearLayoutManager(this@FilmsActivity)
       adapter = filmsAdapter
     }
+  }
 
+  private fun configViewModel() {
     filmsViewModel = ViewModelProviders.of(this).get(FilmsViewModel::class.java)
     filmsViewModel.init(FilmsRepository(FilmsFirebaseSource()))
-    filmsViewModel.getFilms().observeNonNull(this) { filmsAdapter.addItems(it) }
+    filmsViewModel.getFilms().observeNonNull(this) {
+      filmsAdapter.addItems(it)
+      hideLoading()
+    }
   }
 
 }
