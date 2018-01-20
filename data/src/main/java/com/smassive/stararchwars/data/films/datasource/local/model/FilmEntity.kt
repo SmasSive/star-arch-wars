1da70ee804379edc19348d2a4c30fb7dc20ef071
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2018-01-20 19:06:13 +0100

    Convert entity properties to var to generate getters AND setters

--- data/src/main/java/com/smassive/stararchwars/data/films/datasource/local/model/FilmEntity.kt
+++ data/src/main/java/com/smassive/stararchwars/data/films/datasource/local/model/FilmEntity.kt
@@ -4,18 +4,18 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "film")
-data class FilmEntity(@PrimaryKey(autoGenerate = true) val id: Int = 0,
-                      val characters: List<String> = arrayListOf(),
-                      val created: String = "",
-                      val director: String = "",
-                      val edited: String = "",
-                      val opening_crawl: String = "",
-                      val planets: List<String> = arrayListOf(),
-                      val poster: String = "",
-                      val producer: String = "",
-                      val release_date: String = "",
-                      val species: List<String> = arrayListOf(),
-                      val starships: List<String> = arrayListOf(),
-                      val title: String = "",
-                      val url: String = "",
-                      val vehicles: List<String> = arrayListOf())
+data class FilmEntity(@PrimaryKey(autoGenerate = true) var id: Int = 0,
+                      var characters: List<String> = arrayListOf(),
+                      var created: String = "",
+                      var director: String = "",
+                      var edited: String = "",
+                      var opening_crawl: String = "",
+                      var planets: List<String> = arrayListOf(),
+                      var poster: String = "",
+                      var producer: String = "",
+                      var release_date: String = "",
+                      var species: List<String> = arrayListOf(),
+                      var starships: List<String> = arrayListOf(),
+                      var title: String = "",
+                      var url: String = "",
+                      var vehicles: List<String> = arrayListOf())
